0b20d3a65accf95cf4d39e45d38aac9927e53eb6
flamewing <flamewing.sonic@gmail.com>
2021-02-09 15:00:32 +0100

    Removing unused import

--- mdcolors.py
+++ mdcolors.py
@@ -21,7 +21,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from gimpfu import error
 from gimpfu import gimp
 from gimpfu import pdb
 from gimpfu import register
